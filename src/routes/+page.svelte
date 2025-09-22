c3fdc9a34a42e8f20ffdf7e26cfa14fe04444528
abdel-17 <abdelrahmanbahaa@proton.me>
2025-09-22 15:44:26 +0300

    clarify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <!-- everything works when the script tag is at the top -->
 <!-- <script lang="ts"></script> -->
 
-<!-- this is not -->
 {#snippet foo({ props }: { props?: Record<string, unknown> })}{/snippet}
+
+<!-- this won't error if you uncomment the snippet above -->
+{#snippet bar(props: Record<string, unknown>)}{/snippet}
 
 <!-- but not when it's at the bottom -->
 <script lang="ts"></script>
